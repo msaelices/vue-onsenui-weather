6072c6e8fb985e54b66a2f22376a7c8bd06255ea
msaelices <msaelices@gmail.com>
2017-11-26 10:37:56 +0100

    Add vuex-persist to persist the vuex store into localStorage (i.e. the locations and latest forecast).

--- src/store.js
+++ src/store.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist';
 
 import { queryWeather } from '~/api';
 
 Vue.use(Vuex)
+
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex', // The key to store the state on in the storage provider.
+  storage: window.localStorage, // or window.sessionStorage or localForage
+  // Function that passes the state and returns the state with only the objects you want to store.
+  // reducer: state => state,
+  // Function that passes a mutation and lets you decide if it should update the state in localStorage.
+  // filter: mutation => (true)
+})
 
 export default new Vuex.Store({
   modules: {
@@ -46,5 +56,6 @@ export default new Vuex.Store({
         }
       },
     }
-  }
+  },
+  plugins: [vuexLocalStorage.plugin],
 })
