5fb639e6be5fede41b9121fdbe446e5edcb7f612
msaelices <msaelices@gmail.com>
2017-12-06 01:08:41 +0100

    Restrict state storage in localStorage only to the forecast locations. This I guess will fix some navigations issues.

--- src/store.js
+++ src/store.js
@@ -10,7 +10,13 @@ const vuexLocalStorage = new VuexPersist({
   key: 'vuex', // The key to store the state on in the storage provider.
   storage: window.localStorage, // or window.sessionStorage or localForage
   // Function that passes the state and returns the state with only the objects you want to store.
-  // reducer: state => state,
+  reducer: state => {
+    return {
+      forecast: {
+        locations: state.forecast.locations,
+      },
+    };
+  },
   // Function that passes a mutation and lets you decide if it should update the state in localStorage.
   // filter: mutation => (true)
 })
