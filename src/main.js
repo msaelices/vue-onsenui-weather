048c773b2bb09ae5b2aa9cbe0ec464a62bfabbde
msaelices <msaelices@gmail.com>
2017-12-10 23:21:37 +0100

    Refactored imports from Onsen UI in order to bundle only used components.

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import '~/app.css'
 import '~/assets/animated-icons/icons.css';
 
 import Vue from 'vue'
-import VueOnsen from 'vue-onsenui'
+import VueOnsen from 'vue-onsenui/esm'
+import * as VOns from '~/ons-components.js';
 
 import router from '~/router'
 import store from '~/store'
 import App from '~/App.vue'
-import loader  from '~/lib-loader'
+import loader from '~/lib-loader'
 import { Forecast, WeatherIcon } from '~/components'
 import { WeatherPage, MenuPage, HomePage } from '~/pages'
 
@@ -22,9 +23,14 @@ loader.load({
   libraries: ['places'],
 })
 
-for (let c of [Forecast, WeatherPage, MenuPage, HomePage, WeatherIcon]) {
-  Vue.component(c.name, c);
-}
+// registering vue components
+Object.values(VOns).forEach(c => Vue.component(c.name, c));
+Object.values([
+  Forecast,
+  WeatherPage,
+  MenuPage,
+  HomePage,
+  WeatherIcon]).forEach(c => Vue.component(c.name, c));
 
 /* eslint-disable no-new */
 new Vue({
